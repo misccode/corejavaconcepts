458ab569961427f449f5c41847a402fe9abef414
misccode <miscuseage@gmail.com>
2020-08-16 01:59:16 +0530

    First Commit to Git

--- src/com/corejavalessons/TestGit.java
+++ src/com/corejavalessons/TestGit.java
@@ -3,6 +3,6 @@ package com.corejavalessons;
 public class TestGit {
 
     public static void main(String[] args) {
-	System.out.print("Hello Git");
+	System.out.print("First push to git");
     }
 }
